20036644641c9e79b72d6c98023a9ba3a82e0ff0
Valentin Zelionii <32639356+SncBarrington@users.noreply.github.com>
2017-12-30 16:42:14 +0100

    Modify Animation class
    -Added Sprite Getter

---
+++ dev/Animation.h
@@ -0,0 +1,27 @@
+//
+// Created by kairod on 28/12/17.
+//
+
+#ifndef DEV_ANIMATION_H
+#define DEV_ANIMATION_H
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <vector>
+
+class Animation {
+    private:
+        float frame;
+        float speed;
+        sf::Sprite sprite;
+        std::vector<sf::Rect<int> > frames;
+
+    public:
+        Animation(sf::Texture &texture, int x, int y, int width, int height, int count, float speedValue);
+        void update();
+        bool isEnd();
+        sf::Sprite getSprite() const;
+};
+
+
+#endif //DEV_ANIMATION_H
